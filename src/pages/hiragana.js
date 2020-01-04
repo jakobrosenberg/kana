069c0800469736955503a8fecb2fbdb8f7a248c1
Wolfr <johan.ronsse@gmail.com>
2020-01-04 18:17:39 +0100

    First commit

---
+++ src/pages/hiragana.js
@@ -0,0 +1,422 @@
+export default [
+  {
+      "character": "あ",
+      "romaji":"a",
+  },
+  {
+      "character": "い",
+      "romaji":"i",
+  },
+  {
+      "character": "う",
+      "romaji":"u",
+  },
+  {
+      "character": "え",
+      "romaji":"e",
+  },
+  {
+      "character": "お",
+      "romaji":"o",
+  },
+
+  // Second row
+
+  {
+      "character": "か",
+      "romaji":"ka",
+  },
+  {
+      "character": "き",
+      "romaji":"ki",
+  },
+  {
+      "character": "く",
+      "romaji":"ku",
+  },
+  {
+      "character": "け",
+      "romaji":"ke",
+  },
+  {
+      "character": "こ",
+      "romaji":"ko",
+  },
+
+  // Third row
+
+  {
+      "character": "さ",
+      "romaji":"sa",
+  },
+  {
+      "character": "し",
+      "romaji":"shi",
+  },
+  {
+      "character": "す",
+      "romaji":"su",
+  },
+  {
+      "character": "せ",
+      "romaji":"se",
+  },
+  {
+      "character": "そ",
+      "romaji":"so",
+  },
+
+  // Fourth row
+
+  {
+      "character": "た",
+      "romaji":"ta",
+  },
+  {
+      "character": "ち",
+      "romaji":"chi",
+  },
+  {
+      "character": "つ",
+      "romaji":"tsu",
+  },
+  {
+      "character": "て",
+      "romaji":"te",
+  },
+  {
+      "character": "と",
+      "romaji":"to",
+  },
+
+  // Fifth row
+
+  {
+      "character": "な",
+      "romaji":"na",
+  },
+  {
+      "character": "に",
+      "romaji":"ni",
+  },
+  {
+      "character": "ぬ",
+      "romaji":"nu",
+  },
+  {
+      "character": "ね",
+      "romaji":"ne",
+  },
+  {
+      "character": "の",
+      "romaji":"no",
+  },
+
+  // Sixth row
+
+  {
+      "character": "は",
+      "romaji":"ha",
+  },
+  {
+      "character": "ひ",
+      "romaji":"hi",
+  },
+  {
+      "character": "ふ",
+      "romaji":"fu",
+  },
+  {
+      "character": "へ",
+      "romaji":"he",
+  },
+  {
+      "character": "ほ",
+      "romaji":"ho",
+  },
+
+  // Seventh row
+
+  {
+      "character": "ま",
+      "romaji":"ma",
+  },
+  {
+      "character": "み",
+      "romaji":"mi",
+  },
+  {
+      "character": "む",
+      "romaji":"mu",
+  },
+  {
+      "character": "め",
+      "romaji":"me",
+  },
+  {
+      "character": "も",
+      "romaji":"mo",
+  },
+
+  // Eighth row
+
+  {
+      "character": "や",
+      "romaji":"ya",
+  },
+  {
+      "character": "",
+      "romaji":"",
+  },
+  {
+      "character": "ゆ",
+      "romaji":"yu",
+  },
+  {
+      "character": "",
+      "romaji":"",
+  },
+  {
+      "character": "よ",
+      "romaji":"yo",
+  },
+
+  // Eighth row
+
+  {
+      "character": "ら",
+      "romaji":"ra",
+  },
+  {
+      "character": "り",
+      "romaji":"ri",
+  },
+  {
+      "character": "る",
+      "romaji":"ru",
+  },
+  {
+      "character": "れ",
+      "romaji":"re",
+  },
+  {
+      "character": "ろ",
+      "romaji":"ro",
+  },
+
+  // Eighth row
+
+  {
+      "character": "わ",
+      "romaji":"wa",
+  },
+
+  {
+      "character": "",
+      "romaji":"",
+  },
+
+  {
+      "character": "",
+      "romaji":"",
+  },
+
+  {
+      "character": "",
+      "romaji":"",
+  },
+
+  {
+      "character": "を",
+      "romaji":"wo",
+  },
+
+  // 9
+
+  {
+      "character": "",
+      "romaji":"",
+  },
+
+  {
+      "character": "",
+      "romaji":"",
+  },
+
+  {
+      "character": "ん",
+      "romaji":"n",
+  },
+
+  {
+      "character": "",
+      "romaji":"",
+  },
+
+  {
+      "character": "",
+      "romaji":"",
+  },
+
+  // DIACRITICS
+
+  // G
+
+  {
+      "character": "が",
+      "romaji":"ga",
+  },
+
+  {
+      "character": "ぎ",
+      "romaji":"gi",
+  },
+
+  {
+      "character": "ぐ",
+      "romaji":"gu",
+  },
+
+  {
+      "character": "げ",
+      "romaji":"ge",
+  },
+
+  {
+      "character": "ご",
+      "romaji":"go",
+  },
+
+  // Z
+
+  {
+      "character": "ざ",
+      "romaji":"za",
+  },
+
+  {
+      "character": "じ",
+      "romaji":"ji",
+  },
+
+  {
+      "character": "ず",
+      "romaji":"zu",
+  },
+
+  {
+      "character": "ぜ",
+      "romaji":"ze",
+  },
+
+  {
+      "character": "ぞ",
+      "romaji":"zo",
+  },
+
+  // D
+
+  {
+      "character": "だ",
+      "romaji":"da",
+  },
+
+  {
+      "character": "ぢ",
+      "romaji":"ji",
+  },
+
+  {
+      "character": "づ",
+      "romaji":"dzu",
+  },
+
+  {
+      "character": "で",
+      "romaji":"de",
+  },
+
+  {
+      "character": "ど",
+      "romaji":"do",
+  },
+
+  // B
+
+  {
+      "character": "ば",
+      "romaji":"ba",
+  },
+
+  {
+      "character": "び",
+      "romaji":"bi",
+  },
+
+  {
+      "character": "ぶ",
+      "romaji":"bu",
+  },
+
+  {
+      "character": "べ",
+      "romaji":"be",
+  },
+
+  {
+      "character": "ぼ",
+      "romaji":"bo",
+  },
+
+  // P
+
+  {
+      "character": "ぱ",
+      "romaji":"pa",
+  },
+
+  {
+      "character": "ぴ",
+      "romaji":"pi",
+  },
+
+  {
+      "character": "ぷ",
+      "romaji":"pu",
+  },
+
+  {
+      "character": "ぺ",
+      "romaji":"pe",
+  },
+
+  {
+      "character": "ぽ",
+      "romaji":"po",
+  },
+
+  // V
+
+  {
+      "character": "",
+      "romaji":"",
+  },
+
+  {
+      "character": "",
+      "romaji":"",
+  },
+  {
+      "character": "ゔ",
+      "romaji":"vu",
+  },
+  {
+      "character": "",
+      "romaji":"",
+  },
+  {
+      "character": "",
+      "romaji":"",
+  },
+];
+
---
+++ src/pages/index.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Hiragana from './hiragana.js'
+</script>
+
+<style>
+    .c-character-grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    }
+
+    .c-character-grid__item {
+        border-bottom: 1px solid #CCC;
+        padding: 2rem;
+        border-right: 1px solid #CCC;
+        text-align: center;
+    }
+
+    .c-character-grid__item:nth-child(5n) {
+        border-right: none;
+    }
+</style>
+
+<ul class="c-character-grid">
+    {#each Hiragana as character }
+        <li class="c-character-grid__item">
+            <a href="/character/{character.romaji}">{character.character}</a>
+            <!-- {character.romaji} -->
+        </li>
+    {/each}
+</ul>
